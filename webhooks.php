6d3c758d3378fc9aca23450f2792602f1cddce92
casinobot <39646559+casinobot@users.noreply.github.com>
2018-05-26 18:34:30 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '39b8Kvo72cNV+WCaYMPLzdQpYCpjCWtANVmdgs/koNKIAunT0CtAD4p2NYDallTCdC+gpL44kRY+YyMjYE9YTOdVoJZTAcX/kHyydVODzsQEsWUR0AP92ZBXC8/Kk9tImGYVEwn2UkFir/GF1XObPQAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
