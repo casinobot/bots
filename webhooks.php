04fa82d7baa37e7d6e55becc81056ea3eff32c86
casinobot <39646559+casinobot@users.noreply.github.com>
2018-05-30 14:56:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = "สวัสดี : ".$event['source']['userId'].$event['message']['text'];
+			$text = "สวัสดี : ".$event['source']['userId']."-->".$event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
