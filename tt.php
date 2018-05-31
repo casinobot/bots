b7d6472ad66ff99eb5efe30feb671c247e89d43e
casinobot <39646559+casinobot@users.noreply.github.com>
2018-05-31 15:10:31 +0700

    Update tt.php

--- tt.php
+++ tt.php
@@ -13,16 +13,16 @@ $uid="U1cc479767864cf8c57629e6b1623e2f6";
 $url = 'https://api.line.me/v2/bot/profile/'.$uid;
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
-/*curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-*/
 $result = curl_exec($ch);
 echo curl_error($ch);
 curl_close($ch);
 $jsuser=json_decode($result, true);
 echo "ok tt2<br>";
 print_r($jsuser);
+echo "<br><br>".$jsuser['displayName']."<br>";
 ?>
