aaef6de35b173ef6a0034840d915caf9363d95e4
casinobot <39646559+casinobot@users.noreply.github.com>
2018-05-31 15:15:28 +0700

    Update tt.php

--- tt.php
+++ tt.php
@@ -9,20 +9,39 @@ $access_token = '9b8Kvo72cNV+WCaYMPLzdQpYCpjCWtANVmdgs/koNKIAunT0CtAD4p2NYDallTC
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-$uid="U1cc479767864cf8c57629e6b1623e2f6";
-$url = 'https://api.line.me/v2/bot/profile/'.$uid;
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-echo curl_error($ch);
-curl_close($ch);
-$jsuser=json_decode($result, true);
-echo "ok tt2<br>";
-print_r($jsuser);
-echo "<br><br>".$jsuser['displayName']."<br>";
+
+// $uid="U1cc479767864cf8c57629e6b1623e2f6";
+// $url = 'https://api.line.me/v2/bot/profile/'.$uid;
+// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// $ch = curl_init($url);
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// $result = curl_exec($ch);
+// echo curl_error($ch);
+// curl_close($ch);
+// $jsuser=json_decode($result, true);
+// echo "ok tt2<br>";
+// print_r($jsuser);
+// echo "<br><br>".$jsuser['displayName']."<br>";
+function regPlay($uid){
+  // $uid="U1cc479767864cf8c57629e6b1623e2f6";
+  $url = 'https://api.line.me/v2/bot/profile/'.$uid;
+  $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $result = curl_exec($ch);
+  // echo curl_error($ch);
+  curl_close($ch);
+  $jsuser=json_decode($result, true);
+  return $jsuser['displayName'];
+  // echo "ok tt2<br>";
+  // print_r($jsuser);
+}
 ?>
