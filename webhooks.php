d878f90999d7ad840cbc86201666b4c5c77ff27f
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-06 09:55:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,6 +1,6 @@
 <?php // callback.php
 include 'botApi.php';
-require "vendor/autoload.php";
+//require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'R+ksiFqfirX6YbUbLrX0wURJYEBj1gAq+w4PyZrUxVy+e6cKowvlPd/iwkbtnyZXC+gpL44kRY+YyMjYE9YTOdVoJZTAcX/kHyydVODzsQFullnczyaS6FN7m5rqbAlFMp6Gv0ik1Z+FWc4Y6+JufQdB04t89/1O/w1cDnyilFU=';
