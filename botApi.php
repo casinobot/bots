ef44b09a657276badfc2af5565ca0b0d826446bd
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-11 13:21:36 +0700

    Update botApi.php

--- botApi.php
+++ botApi.php
@@ -31,10 +31,11 @@ function regPlay($uid){
   $sqlAdduser="INSERT INTO tb_user values(null,'$name','$u_id','$urlpic','-')";
   $queryAdduser=mysqli_query($con,$sqlAdduser);
 //   echo "<script>window.location='http://www.casinopanels.com/services/api.php?func=addUser&user=+".$u_id."+&name=+".$name."+&urlpic=+".$urlpic."+&statMsg=+".$statMsg."+';</script>";
-  header('Location: http://www.casinopanels.com/services/api.php?func=addUser&user='.$u_id.'&name='.$name.'&urlpic='.$urlpic.'&statMsg='.$statMsg);
+  
 /*---------------------  เอาค่าใน $jsuser ลง DB  --------------------*/
   $user=$name;
   return $user;
+  header('Location: http://www.casinopanels.com/services/api.php?func=addUser&user='.$u_id.'&name='.$name.'&urlpic='.$urlpic.'&statMsg='.$statMsg);
 }
-echo "tt ok2";
+echo "tt ok";
 ?>
