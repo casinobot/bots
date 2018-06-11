ebacfcac85d7cf0a23af0cb30024c9a363652e55
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-11 10:47:47 +0700

    Update botApi.php

--- botApi.php
+++ botApi.php
@@ -9,8 +9,8 @@ $access_token = 'R+ksiFqfirX6YbUbLrX0wURJYEBj1gAq+w4PyZrUxVy+e6cKowvlPd/iwkbtnyZ
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-function regPlay($uid){
-//   $uid="U1cc479767864cf8c57629e6b1623e2f6";
+// function regPlay($uid){
+  $uid="U1cc479767864cf8c57629e6b1623e2f6";
   $url = 'https://api.line.me/v2/bot/profile/'.$uid;
   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);
@@ -29,10 +29,10 @@ function regPlay($uid){
   $urlpic=$user['pictureUrl'];
   $statMsg=$user['statusMessage'];
 
-  header('Location: http://www.casinopanels.com/services/api.php?func=addUser&user=$u_id&name=$name&urlpic=$urlpic&statMsg=$statMsg');
+  echo 'Location: http://www.casinopanels.com/services/api.php?func=addUser&user=$u_id&name=$name&urlpic=$urlpic&statMsg=$statMsg';
 /*---------------------  เอาค่าใน $jsuser ลง DB  --------------------*/
   $user=$uid;
-  return $user;
-}
+//   return $user;
+// }
 echo "tt ok";
 ?>
