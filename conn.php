12aafd47680358400a9c1a7503e025c7460c1118
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-11 13:13:43 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
     global $con;
     $user="root";
     $pass="6d51b9e1e78a2ef67e414eadbca33172019fd1cf913a0f2d";
-    $host="localhost";
+    $host="206.189.148.135";
     $db="casino_panel";
     $con = mysqli_connect($host,$user,$pass,$db);
     mysqli_set_charset($con,"utf8");
