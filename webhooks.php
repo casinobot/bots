f3303efbfc2a30d06ff5413a39e16932f6e734c6
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-11 16:09:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -11,49 +11,78 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			// $text = "สวัสดี : ".$event['source']['userId']."-->".$event['message']['text'];
-			$text="สวัสดี";
-// 			$a_n="Aone";
-// 			$re=regPlay($a_n);
-// 			$text=$text."คุณ : ".$re;
-			if($event['message']['text']=="play"){
-				$u_id=$event['source']['userId'];
-				$re=regPlay($u_id);
-				$text=$text."คุณ : ".$re;
-			}
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            // $text = "สวัสดี : ".$event['source']['userId']."-->".$event['message']['text'];
+            switch ($event['source']['type']) {
+                case 'user':
+                 $messages = [
+                  'type' => 'text',
+                  // 'text' => $ms
+                  'text' => 'userId: '.$event['source']['userId']
+                 ];
+                 break;
+                case 'room':
+                 $messages = [
+                  'type' => 'text',
+                  // 'text' => $ms
+                  'text' => 'roomId: '.$event['source']['roomId'].' userId: '.$event['source']['userId']
+                 ];
+                 break;
+                case 'group':
+                    // if($event['message']['text']=="play"){
+                    //     $u_id=$event['source']['userId'];
+                    //     $re=regPlay($u_id);
+                    //     $text=$text."คุณ : ".$re;
+                    // }
+                     $messages = [
+                      'type' => 'text',
+                      // 'text' => $ms
+                      'text' => 'groupId: '.$event['source']['groupId']
+                     ];
+                     break;
+                
+                    default:
+                 # code...
+                 break;                 
+             }
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            $text="สวัสดี";
+//          $a_n="Aone";
+//          $re=regPlay($a_n);
+//          $text=$text."คุณ : ".$re;
+            
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			echo $result . "\r\n";
-		}
-	}
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }
+    }
 }
-echo "OK";
+echo "OK1";
