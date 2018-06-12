625421651bd044334861b803bdcd025bb8b05b55
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-12 21:55:10 +0700

    Update botApi.php

--- botApi.php
+++ botApi.php
@@ -3,13 +3,14 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'R+ksiFqfirX6YbUbLrX0wURJYEBj1gAq+w4PyZrUxVy+e6cKowvlPd/iwkbtnyZXC+gpL44kRY+YyMjYE9YTOdVoJZTAcX/kHyydVODzsQFullnczyaS6FN7m5rqbAlFMp6Gv0ik1Z+FWc4Y6+JufQdB04t89/1O/w1cDnyilFU=';
+
 $name="Aone";
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 function regPlay($uid){
+  $access_token = 'R+ksiFqfirX6YbUbLrX0wURJYEBj1gAq+w4PyZrUxVy+e6cKowvlPd/iwkbtnyZXC+gpL44kRY+YyMjYE9YTOdVoJZTAcX/kHyydVODzsQFullnczyaS6FN7m5rqbAlFMp6Gv0ik1Z+FWc4Y6+JufQdB04t89/1O/w1cDnyilFU=';
 //   $uid="U1cc479767864cf8c57629e6b1623e2f6";
   $userId=(string)$uid;
   $url = 'https://api.line.me/v2/bot/profile/'.$userId;
@@ -39,5 +40,5 @@ function regPlay($uid){
   return $user;
   
 }
-echo "1";
+echo "2";
 ?>
