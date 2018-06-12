0b052543f493a2986cd2fad6f9539b4e37f1492a
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-12 22:56:16 +0700

    Update botApi.php

--- botApi.php
+++ botApi.php
@@ -12,9 +12,9 @@ $events = json_decode($content, true);
 function regPlay($uid){
   global $access_token;
 //   $uid="U1cc479767864cf8c57629e6b1623e2f6";
-//   $userId=(string)$uid;
-//   $url = 'https://api.line.me/v2/bot/profile/'.$userId;
-  $url = 'https://api.line.me/v2/bot/group/Cca768dbe769160b0bd462af687250b7f/members/ids';
+  $userId=(string)$uid;
+  $url = 'https://api.line.me/v2/bot/profile/'.$userId;
+//   $url = 'https://api.line.me/v2/bot/group/Cca768dbe769160b0bd462af687250b7f/members/ids';
   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
