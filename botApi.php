d821e8237b2498fe20ebbf9f7d2a6480cc713d29
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-14 11:47:47 +0700

    Update botApi.php

--- botApi.php
+++ botApi.php
@@ -1,5 +1,5 @@
 <?php // callback.php
-// include 'conn.php';
+include '../services/api.php';
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = 'R+ksiFqfirX6YbUbLrX0wURJYEBj1gAq+w4PyZrUxVy+e6cKowvlPd/iwkbtnyZXC+gpL44kRY+YyMjYE9YTOdVoJZTAcX/kHyydVODzsQFullnczyaS6FN7m5rqbAlFMp6Gv0ik1Z+FWc4Y6+JufQdB04t89/1O/w1cDnyilFU=';
@@ -26,19 +26,28 @@ function regPlay($uid){
   echo curl_error($ch);
   curl_close($ch);
   $jsuser=json_decode($result, true);
-//   echo $jsuser['displayName'];
   $u_id=$jsuser['userId'];
   $name=$jsuser['displayName'];
   $urlpic=$jsuser['pictureUrl'];
   $statMsg=$jsuser['statusMessage'];
-//   $sqlAdduser="INSERT INTO tb_user values(null,'$name','$u_id','$urlpic','-')";
-//   $queryAdduser=mysqli_query($con,$sqlAdduser);
-//   echo "<script>window.location='http://www.casinopanels.com/services/api.php?func=addUser&user=+".$u_id."+&name=+".$name."+&urlpic=+".$urlpic."+&statMsg=+".$statMsg."+';</script>";
-//   header('Location: http://www.casinopanels.com/services/api.php?func=addUser&user='.$u_id.'&name='.$name.'&urlpic='.$urlpic.'&statMsg='.$statMsg);
+  addUser($u_id,$name,$urlpic,$statMsg);
 /*---------------------  เอาค่าใน $jsuser ลง DB  --------------------*/
   $user=$name;
   return $user;
   
 }
-echo "2";
+echeckMoney($uId){
+  global $con;
+  $selectUser="SELECT * from tb_user where user_token='$user'";
+  $queryUser=mysqli_query($con,$selectUser);
+  $rowUser=mysqli_fetch_array($queryUser);
+  $selectAcc="SELECT * from tb_account where user_id='$rowUser[user_id]'";
+  $queryAcc=mysqli_query($con,$selectAcc);
+  $rowAcc=mysqli_fetch_array($queryAcc);
+  // $money=$rowAcc['account_total'];
+  $money=100;
+  return $money;
+  // $numAcc=$queryAcc->num_rows;
+}
+echo "bot Api<br>";
 ?>
