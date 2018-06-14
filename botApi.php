fd31704b172a8ef5859c12d38d2ea628f6106239
casinobot <39646559+casinobot@users.noreply.github.com>
2018-06-14 11:52:56 +0700

    Update botApi.php

--- botApi.php
+++ botApi.php
@@ -37,14 +37,14 @@ function regPlay($uid){
   
 }
 function checkMoney($uId){
-  global $con;
+  /*global $con;
   $selectUser="SELECT * from tb_user where user_token='$user'";
   $queryUser=mysqli_query($con,$selectUser);
   $rowUser=mysqli_fetch_array($queryUser);
   $selectAcc="SELECT * from tb_account where user_id='$rowUser[user_id]'";
   $queryAcc=mysqli_query($con,$selectAcc);
   $rowAcc=mysqli_fetch_array($queryAcc);
-  $money=$rowAcc['account_total'];
+  $money=$rowAcc['account_total'];*/
   $money=100;
   return $money;
   // $numAcc=$queryAcc->num_rows;
